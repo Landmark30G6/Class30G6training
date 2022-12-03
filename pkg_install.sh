6bd64214cdc96d1ebc77ff1e6a486ee462f28917
elvis.ngeh <evis.bantar@gmail.com>
2022-12-02 23:45:25 -0500

    pkg_install

---
+++ pkg_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "what pkg do you want to install?"
+read pkg 
+sudo yum install $pkg -y >> /tmp/yum.log
+sudo cat /tmp/yum.log | grep "Nothing to do"
+if [[ $message == "Nothing to do" ]] 
+then
+message="Nothing to do"
+else
+echo "continue with the installation"
+fi
+echo "pls enter ur pkg again or a new one"
+read pkg
+sudo yum install $pkg -y >> /tmp/yum.log
+sudo cat /tmp/yum.log | grep "Complete!"
+if [[ $message == "Complete!" ]]
+then
+message="Complete!"
+else
+message="Nothing to do"
+echo "your pkg is installed"
+fi
